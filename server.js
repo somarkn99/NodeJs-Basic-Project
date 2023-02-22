048a393b730c14e3e51b8d8e9911635ca6884eec
Somar <kesen.somar.99@gmail.com>
2023-02-22 14:43:08 +0300

    Feat (package): install mongoose for DB

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const dbConnection = require('./config/database');
 
 dotenv.config({path:'config.env'});
+
+// DB Connection Configuration
+dbConnection();
+
 
 // express app
 const app = express();
 
+// Middleware
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
     console.log(`You App is running in ${process.env.NODE_ENV} mode`);
