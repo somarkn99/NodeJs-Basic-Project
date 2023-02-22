5a40473790891c41c08d03446675df72eab64666
Somar <kesen.somar.99@gmail.com>
2023-02-22 14:14:05 +0300

    Feat (package): install morgan to make logging

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+
 dotenv.config({path:'config.env'});
 
 // express app
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+    console.log(`You App is running in ${process.env.NODE_ENV} mode`);
+}
+
 
 const PORT = process.env.PORT || 8000;
 
