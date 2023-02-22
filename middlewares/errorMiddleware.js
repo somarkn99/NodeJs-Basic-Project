145fcd1c01206c0df6bea3e30bd8ce58ef3044bc
Somar <kesen.somar.99@gmail.com>
2023-02-22 17:27:42 +0300

    Feat: Handle error in dev & prod env

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,8 +1,6 @@
 const ApiError = require("../utils/apiError");
 
-const globalError = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+const sendErrorForDev = (err, res) =>
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
@@ -10,6 +8,22 @@ const globalError = (err, req, res, next) => {
     // @desc: Where the error happen
     stack: err.stack,
   });
+
+const sendErrorForProd = (err, res) =>
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+
+const globalError = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorForDev(err, res);
+  } else {
+    sendErrorForProd(err, res);
+  }
 };
 
 module.exports = globalError;
